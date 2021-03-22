2b6fdcde126b732ff3b755a0538ad6bb5fe966cb
Pham Thai Son <swlpham@gmail.com>
2021-03-23 00:14:20 +0700

    done multihead attention

--- src/transformer_modules.py
+++ src/transformer_modules.py
@@ -25,6 +25,51 @@ def positional_encoding(x, max_seq_len):
     return tf.cast(pos_encoding, dtype=tf.float32)
 
 
-# def multihead_attention(queries, keys, values,key_masks,num_heads=8,
-#                         dropout_rate=0.3,training=False,causality=True):
-#
+def attention(q, k, v, mask=None, dropout_rate=None):
+    """
+    q: batch_size x head x seq_length x d_model
+    k: batch_size x head x seq_length x d_model
+    v: batch_size x head x seq_length x d_model
+    mask: batch_size x 1 x 1 x seq_length
+    output: batch_size x head x seq_length x d_model
+    """
+
+    # attention score được tính bằng cách nhân q với k
+    d_k = q.size(-1)
+    scores = tf.linalg.matmul(q, k.transpose(-2, -1)) / tf.math.sqrt(d_k))
+
+    if mask is not None:
+        mask = tf.expand_dims(mask,1)
+        scores = tf.where(mask == 0, -1e9, mask)
+    # xong rồi thì chuẩn hóa bằng softmax
+    scores = tf.nn.softmax(scores, dim=-1)
+
+    if dropout_rate is not None:
+        scores = tf.nn.dropout(scores, dropout_rate)
+
+    output = tf.linalg.matmul.matmul(scores, v)
+    return output, scores
+
+def multihead_attention(queries, keys, values, key_masks, num_heads=8,
+                        dropout_rate=0.3,training=False,causality=True):
+    shape = queries.get_shape().as_list()
+    d_model = shape[-1]
+    assert d_model % num_heads == 0
+    d_k = d_model // num_heads
+    bs = shape[0]
+
+    q_linear = tf.layers.dense(queries, trainable=training)
+    q_linear = tf.reshape(q_linear, tf.constant([bs, -1, num_heads, d_k]))
+    k_linear = tf.layers.dense(keys, trainable=training)
+    k_linear = tf.reshape(k_linear, tf.constant([bs, -1, num_heads, d_k]))
+    v_linear = tf.layers.dense(values, trainable=training)
+    v_linear = tf.reshape(v_linear, tf.constant([bs, -1, num_heads, d_k]))
+
+    q = tf.transpose(q_linear)
+    v = tf.transpose(v_linear)
+    k = tf.transpose(k_linear)
+
+    scores, attn = attention(q, k, v, key_masks, dropout_rate)
+
+    concat = tf.transpose(tf.reshape(scores, tf.constant([bs, -1, num_heads, d_k])))
+    return tf.layers.dense(concat)
